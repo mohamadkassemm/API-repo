336d97866ef0653898e62d32058603d6f53e5332
Mohammad Kassem <mohammaddkassemm@gmail.com>
2022-09-13 09:36:41 +0300

    adding json to API2

--- files/API2.php
+++ files/API2.php
@@ -3,11 +3,14 @@
 $a = $_POST["a"];
 $b = $_POST["b"];
 $c = $_POST["c"];
-if($b==0){
-    $result= "Math error";
-}else{
-    $result = $a**3 + $b*c - $a/$b;
-}
 
+if($b==0){
+    $total= "Math error";
+}else{
+    $total = $a**3 + $b*c - $a/$b;
+}
+$result=[
+    "total" => $total,
+];
 echo json_encode($result);
 ?>
