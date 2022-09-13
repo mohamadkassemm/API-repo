b9ea1add51c9f847e717566b0d3307d49d0e1b96
Mohammad Kassem <mohammaddkassemm@gmail.com>
2022-09-13 10:33:42 +0300

    adding else condition to API3

--- files/API3.php
+++ files/API3.php
@@ -2,8 +2,10 @@
 
 $password=$_GET["pass"];
 
-if (strlen($password)>12 & preg_match('/[A-Z]/', $password) & preg_match('/[a-z]/', $password)){
-    echo "Strong password";
+if (strlen($password)>12 & preg_match('/[A-Z]/', $password) & preg_match('/[a-z]/', $password) & preg_match('/[1-9]/', $password)){
+    echo "Your password is :Strong password";
+}else{
+    echo "Your password is :Weak password";
 }
 
 ?>
